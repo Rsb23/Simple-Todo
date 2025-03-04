5ca3aa79359cb0a2f958e58072d277a54f9ac4a1
Rsb23 <rsbarbu23@gmail.com>
2025-03-03 22:55:26 -0500

    added remove_task(), update_task_name(), update_task_desc(), get_all_tasks(), get_task_desc() func database_parser.py

--- src/database_parser.py
+++ src/database_parser.py
@@ -5,8 +5,9 @@ from sqlite3 import Error
 
 
 class DataManagement():
-    def __init__(self):
-        self.conn = self.create_connection("database.db")
+    def __init__(self, filepath):
+        self.conn = self.create_connection(filepath)
+        self.create_table()  # creates table if none exists
 
     def create_connection(self, filepath) -> sqlite3.Connection | ConnectionAbortedError:
         try:
@@ -47,18 +48,59 @@ class DataManagement():
         except sqlite3.OperationalError as e:
             raise e
     
-    def remove_task(self, id: int) -> bool:
-        pass
+    def remove_task(self, name: str) -> None:
+        try:
+            cursor = self.conn.cursor()
+            cursor.execute(
+                """
+                DELETE FROM tasks WHERE name = ?
+                """,
+                (name,)
+            )
+            self.conn.commit()
+        except sqlite3.OperationalError as e:
+            raise e
 
-    def update_task_name(self, name: str) -> bool:
-        pass
+    def update_task_name(self, target_name: str, name: str) -> None:
+        try:
+            cursor = self.conn.cursor()
+            cursor.execute(
+                """
+                UPDATE tasks SET name = ? WHERE name = ?
+                """,
+                (name, target_name)
+            )
+            self.conn.commit()
+        except sqlite3.OperationalError as e:
+            raise e
 
-    def update_task_desc(self, desc: str) -> bool:
-        pass
+    def update_task_desc(self, target_name: str, desc: str) -> None:
+        try:
+            cursor = self.conn.cursor()
+            cursor.execute(
+                """
+                UPDATE tasks SET desc = ? WHERE name = ?
+                """,
+                (desc, target_name)
+            )
+            self.conn.commit()
+        except sqlite3.OperationalError as e:
+            raise e
 
-
-_DataManagement = DataManagement()
-_DataManagement.create_table()
-_DataManagement.add_task(("First Task", "This is a test description", datetime.now()))
-_DataManagement.add_task(("Second Task", "This is a test description", datetime.now()))
-_DataManagement.add_task(("Third Task", "This is a test description", datetime.now()))
+    def get_all_tasks(self) -> tuple:
+        try:
+            cursor = self.conn.cursor()
+            cursor.execute("SELECT * FROM tasks")
+            rows = cursor.fetchall()
+            return tuple(rows)
+        except sqlite3.OperationalError as e:
+            raise e
+    
+    def get_task_desc(self, name: str) -> str:
+        try:
+            cursor = self.conn.cursor()
+            cursor.execute("SELECT desc FROM tasks WHERE name = ?", (name,))
+            desc = cursor.fetchall()
+            return tuple(desc)
+        except sqlite3.OperationalError as e:
+            raise e
